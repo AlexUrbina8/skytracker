7d1c07c4c12fec64e80f114f6795df9e828c0150
Alejandro <alejandrourbinag8@gmail.com>
2026-04-20 12:25:59 -0600

    Add : Nuevas Clases

---
+++ app/src/main/java/com/example/skytrack/classes/SearchWeatherWidgetClass.kt
@@ -0,0 +1,8 @@
+package com.example.skytrack.classes
+
+data class SearchWeatherWidgetClass(
+    val Id : Int,
+    val City : String,
+    val State : String,
+    val Temperatura : Int
+)
---
+++ app/src/main/java/com/example/skytrack/classes/WeekWeatherWidgetClass.kt
@@ -0,0 +1,9 @@
+package com.example.skytrack.classes
+
+data class WeekWeatherWidgetClass(
+    val Id : Int,
+    val Date : String,
+    val Weather : String,
+    val MinTemperature : Int,
+    val MaxTemperature : Int
+)
